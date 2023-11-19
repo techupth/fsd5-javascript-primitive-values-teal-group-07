5d990577807152e32b90b16fd3e39be235c2fd4f
suphattraying <suphattra.spk@gmail.com>
2023-11-19 15:31:49 +0700

    feat:finish

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let product_price = 27000;
+let discount_percentage = (product_price*0.25);
+let net_price = product_price-discount_percentage;
+console.log(net_price)
+
